6e729ef80b85eb1507bd71f19e3d55239ccabfde
Mayela244 <54426548+Mayela244@users.noreply.github.com>
2020-06-14 05:47:13 -0500

    Create MainActivity.kt

---
+++ PROYECTO - copia/app/src/main/java/com/example/proyecto/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.proyecto
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnregistro.setOnClickListener {
+          var intent:Intent = Intent  (this, MainActivityRegistro::class.java)
+            startActivity(intent)
+        }
+    }
+
+    }
+
+
